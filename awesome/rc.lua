72bc5dcfe034e52e0d8bc9ec61683cbdb688221e
Tymon Thi <tnthi115@berkeley.edu>
2022-10-12 01:32:27 -0700

    Change nitrogen to set wallpaper as zoomed fill instead of scaled.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -738,4 +738,5 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Autostart programs
 awful.spawn.with_shell("compton")
-awful.spawn.with_shell("nitrogen --set-scaled --random /media/sf_Downloads/wallpapers/gruvbox")
+-- awful.spawn.with_shell("nitrogen --set-scaled --random /media/sf_Downloads/wallpapers/gruvbox")
+awful.spawn.with_shell("nitrogen --set-zoom-fill --random /media/sf_Downloads/wallpapers/gruvbox")
