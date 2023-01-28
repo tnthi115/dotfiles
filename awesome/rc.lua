c23aad1151ba28614a9bd8b2ed13650a543b3761
Tymon Thi <tnthi115@berkeley.edu>
2023-01-27 17:54:17 -0800

    Add CS184 drawing windows to floating clients rule.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -637,7 +637,8 @@ awful.rules.rules = {
       "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
       "Wpa_gui",
       "veromix",
-      "xtightvncviewer"
+      "xtightvncviewer",
+      "CS184", -- Float CS184 drawing windows
     },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
