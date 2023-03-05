5b55a500137bee4d1364d3a300d17bf39b7773db
Tymon Thi <tnthi115@berkeley.edu>
2023-03-04 18:43:50 -0800

    Add keymaps for todo-comments.

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -398,6 +398,21 @@ lvim.keys.normal_mode["zk"] = "zckkzOzz"
 -- Close all folds except the current one. [f]ocus the current fold.
 lvim.keys.normal_mode["zf"] = "zMzvzz"
 
+-- todo-comments
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- You can also specify a list of valid jump keywords
+
+-- vim.keymap.set("n", "]t", function()
+--   require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
+-- end, { desc = "Next error/warning todo comment" })
+
 -- }}}
 
 -- Which-key {{{
