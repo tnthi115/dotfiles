e117a4751d457919a05e1516d5b9e945dd04baee
Tymon Thi <tnthi115@berkeley.edu>
2023-03-04 19:20:58 -0800

    Fix todo-comments keyword matching.

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -926,7 +926,7 @@ lvim.plugins = {
           keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
           after = "fg", -- "fg" or "bg" or empty
           -- pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-          pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
+          pattern = [[.*<(KEYWORDS)\s*:*]], -- pattern or table of patterns, used for highlightng (vim regex)
           comments_only = true, -- uses treesitter to match keywords in comments only
           max_line_len = 400, -- ignore lines longer than this
           exclude = {}, -- list of file types to exclude highlighting
@@ -960,7 +960,7 @@ lvim.plugins = {
           -- don't replace the (KEYWORDS) placeholder
           -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
           -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-          pattern = [[\b(KEYWORDS):*\b*]], -- match 0 or more '*' or Unicode word boundary
+          pattern = [[\b(KEYWORDS):*\b]], -- match 0 or more ':' followed by a Unicode word boundary
         },
       }
     end
