e8e7fe53882e8a87820a1b494bd5f8db1d1e6429
Tymon Thi <tnthi115@berkeley.edu>
2023-03-24 12:25:18 -0700

    Add feh to floating clients in awesomewm.

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -794,6 +794,7 @@ awful.rules.rules = {
           "CGL",
           "screengrab",
           "lximage",
+          "feh",
         },
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
