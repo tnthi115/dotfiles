45f6bab098731fcc629950df3da6eedf8d264f52
Tymon Thi <tnthi115@berkeley.edu>
2023-04-15 17:32:08 -0700

    Add firefox hardening files.

---
+++ firefox/user-overrides.js
@@ -0,0 +1,7 @@
+user_pref("keyword.enabled", true);
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
+user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.startup.page", 1); // open home on startup
+user_pref("browser.startup.homepage", "about:home"); // default homepage, no blank
