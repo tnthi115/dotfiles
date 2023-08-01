be4a360e53d349d158bf71d4e7128b2e2ba4cae4
Tymon Thi <tnthi115@berkeley.edu>
2023-08-01 15:41:14 -0700

    Restructure scripts in bin/ and add bin/raycast for all raycast scripts.

---
+++ bin/raycast/run-shell-command.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Run shell command
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "command" }
+
+# Documentation:
+# @raycast.description Run a shell command in the terminal
+# @raycast.author Tymon
+
+# echo "Hello World! Argument1 value: "$1""
+
+# echo "all arguments:" "$@"
+
+wezterm start -- $@
+
+# WIP: this currently doesn't have the same environment as a normal terminal (not same login shell?)
