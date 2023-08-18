f198c2fc8c52fed013cdbda460830fd9a62e6b0e
Tymon Thi <tnthi115@berkeley.edu>
2023-08-18 12:54:04 -0700

    feat(lvim): add usort for python formatting

--- lvim/.config/lvim/lua/user/lsp/languages/python.lua
+++ lvim/.config/lvim/lua/user/lsp/languages/python.lua
@@ -32,6 +32,7 @@ table.insert(lvim.plugins, {
 ------------------------
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { name = "black" } }
+formatters.setup { { name = "usort" } }
 -- lvim.format_on_save.enabled = true
 -- lvim.format_on_save.pattern = { "*.py" }
 
