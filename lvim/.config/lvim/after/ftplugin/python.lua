b09729a62111aa87084d455ee1d34df4ac360c1e
Tymon Thi <tnthi115@berkeley.edu>
2023-08-28 14:41:05 -0700

    refactor(lvim): add python to description of python debugging keybinds

--- lvim/.config/lvim/after/ftplugin/python.lua
+++ lvim/.config/lvim/after/ftplugin/python.lua
@@ -123,12 +123,16 @@ local opts = {
 local mappings = {
   c = {
     name = "Python",
-    -- Switch virtual environment
-    e = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
-    -- Testing
-    m = { "<cmd>lua require('neotest').run.run()<cr>", "Test Method" },
-    c = { "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" },
-    s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" },
+    -- name = "Code",
+    -- p = {
+    --   name = "Python",
+    --   -- Switch virtual environment
+    --   e = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+    --   -- Testing
+    --   m = { "<cmd>lua require('neotest').run.run()<cr>", "Test Method" },
+    --   c = { "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" },
+    --   s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" },
+    -- },
   },
 }
 
@@ -136,8 +140,8 @@ which_key.register(mappings, opts)
 
 -- DAP mappings
 lvim.builtin.which_key.mappings["dm"] =
-  { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Test Method DAP" }
+  { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Test Python Method DAP" }
 lvim.builtin.which_key.mappings["dc"] = {
   "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
-  "Test Class DAP",
+  "Test Python Class DAP",
 }
