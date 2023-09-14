9e3670749b08341d386d9e8dc4900752b5594b84
Tymon Thi <tnthi115@berkeley.edu>
2023-09-14 11:18:02 -0700

    feat: update neogen config with better comments and golang section

--- lvim/.config/lvim/lua/user/neogen.lua
+++ lvim/.config/lvim/lua/user/neogen.lua
@@ -1,18 +1,33 @@
+-- for a full list of annotation_conventions, see supported-languages below
+-- https://github.com/danymat/neogen#supported-languages
+
+-- for more template configurations, see the language's configuration file in
+-- configurations/{lang}.lua
+-- https://github.com/danymat/neogen/tree/main/lua/neogen/configurations
+
 require("neogen").setup {
   enabled = true,
+  input_after_comment = true,
   snippet_engine = "luasnip",
   languages = {
     lua = {
       template = {
-        -- for a full list of annotation_conventions, see supported-languages below,
+        -- emmylua | ldoc
         annotation_convention = "emmylua",
-        -- for more template configurations, see the language's
-        -- configuration file in configurations/{lang}.lua
+        -- lunarvim I think uses ldoc, but this plugin doesn't seem to
+        -- currently have as good support for it
+        -- annotation_convention = "ldoc",
       },
     },
     python = {
       template = {
+        -- google_docstrings | nympydoc | reST
         annotation_convention = "reST",
+      },
+    },
+    go = {
+      template = {
+        annotation_convention = "godoc",
       },
     },
   },
