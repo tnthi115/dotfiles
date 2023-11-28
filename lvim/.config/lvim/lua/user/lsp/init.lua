c36d5182232fad35b3190b5eca2afa8c4234f87f
Tymon Thi <tnthi115@berkeley.edu>
2023-11-27 19:32:32 -0800

    revert(tmux): back to using c-e as prefix key to avoid clash of c-space for nvim-cmp

--- lvim/.config/lvim/lua/user/lsp/init.lua
+++ lvim/.config/lvim/lua/user/lsp/init.lua
@@ -57,10 +57,9 @@ lvim.builtin.treesitter.ensure_installed = {
 -- lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- local cmp = require "cmp"
-
 -- Remap suggestion window close to ctrl + a (from ctrl + e)
--- lvim.keys.insert_mode["<C-a>"] = cmp.mapping.abort()
+local cmp = require "cmp"
+lvim.keys.insert_mode["<C-a>"] = cmp.mapping.abort()
 
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
