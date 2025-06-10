57dda9657927ddd2307640f93cc503d6672f0ba7
Tymon Thi <tnthi115@berkeley.edu>
2025-06-10 10:35:59 -0700

    feat(fish): attempt at adding !! and !$ but not working

---
+++ fish/.config/fish/functions/bang_bang.fish
@@ -0,0 +1,27 @@
+# add fish functions that mimic !! and !$ in bash 
+
+# TODO: doesn't work
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
