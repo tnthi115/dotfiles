caa68d168d229615b890f87d4c34027160ce2424
Tymon Thi <tnthi115@berkeley.edu>
2025-07-01 03:04:13 -0700

    feat(fish): add kanagawa theme

--- fish/.config/fish/conf.d/theme.fish
+++ fish/.config/fish/conf.d/theme.fish
@@ -6,5 +6,5 @@
 # type -q theme_tokyonight || return 1
 # theme_tokyonight night && return 0
 
-set -q MY_THEME; or set -gx MY_THEME tokyonight_night
-fish_config theme choose $MY_THEME
+# set -q MY_THEME; or set -gx MY_THEME tokyonight_night
+# fish_config theme choose $MY_THEME
