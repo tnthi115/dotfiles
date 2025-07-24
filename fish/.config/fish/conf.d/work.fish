660f7d6a81d1409a6d6d03dbd3af6ad93c63dfb8
Tymon Thi <tnthi115@berkeley.edu>
2025-07-24 16:52:39 -0700

    refactor(fish): update work.fish to reflect work-dotfiles fish refactoring

--- fish/.config/fish/conf.d/work.fish
+++ fish/.config/fish/conf.d/work.fish
@@ -1,3 +1,15 @@
-if test -e ~/work-dotfiles/fish/functions/work.fish
-    source ~/work-dotfiles/fish/functions/work.fish
+if test -d ~/work-dotfiles/fish/functions
+    for file in ~/work-dotfiles/fish/functions/*.fish
+        if test -f $file
+            source $file
+        end
+    end
+end
+
+if test -d ~/work-dotfiles/fish/conf.d
+    for file in ~/work-dotfiles/fish/conf.d/*.fish
+        if test -f $file
+            source $file
+        end
+    end
 end
