53391a3e2339d6d11f6b9b047f414dcf3252d93d
Tymon Thi <tnthi115@berkeley.edu>
2026-01-21 16:18:12 -0800

    chore(fish): enable vi keybindings and clean fish variables
    Enable fish_vi_key_bindings and add custom visual/insert bindings. Update fish_variables (bump __fish_initialized and remove stale color/pager vars) to simplify environment state and prefer goenv paths.,

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,10 +1,11 @@
 function fish_user_key_bindings
-    # bind -e --preset \el
-    # bind -M default -e --preset \el
-    bind -M insert -e --preset \el
-    # bind -M visual -e --preset \el
+    # Enable vim mode
+    fish_vi_key_bindings
 
+    # Custom keybindings
+    # Unbind preset \el (Alt+L) in insert mode
+    bind -M insert -e --preset \el
+
+    # Visual mode: V selects entire line
     bind -m visual V beginning-of-line begin-selection end-of-line force-repaint
-    # bind -m visual V commandline --set-selection 0 (commandline -L)
-    # bind -M visual \cC fish_clipboard_copy
 end
